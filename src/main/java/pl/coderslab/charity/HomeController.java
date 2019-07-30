ed6b9ed4e7e641938f7bb90421df3f166c56bac6
Damian Skrzyniarz <skrzyniarz.damian@gmail.com>
2019-07-30 18:49:32 +0200

    Added institution list in home view

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -4,19 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import pl.coderslab.charity.entity.Category;
-import pl.coderslab.charity.repository.CategoryRepository;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import pl.coderslab.charity.institution.InstitutionRepository;
 
 
 @Controller
 public class HomeController {
+    private InstitutionRepository institutionRepository;
+
+    @Autowired
+    public HomeController(InstitutionRepository institutionRepository) {
+        this.institutionRepository = institutionRepository;
+    }
 
     @RequestMapping("/")
     public String homeAction(Model model){
+        model.addAttribute("institutions", institutionRepository.findAll());
         return "index";
     }
 }
