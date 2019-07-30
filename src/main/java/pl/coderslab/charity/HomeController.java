e7620ea2b37234ccd96e69ac91ed7c376975208e
Damian Skrzyniarz <skrzyniarz.damian@gmail.com>
2019-07-30 20:00:10 +0200

    Added sum of donated items to front page view

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -4,21 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.donation.DonationRepository;
 import pl.coderslab.charity.institution.InstitutionRepository;
 
 
 @Controller
 public class HomeController {
     private InstitutionRepository institutionRepository;
+    private DonationRepository donationRepository;
 
     @Autowired
-    public HomeController(InstitutionRepository institutionRepository) {
+    public HomeController(InstitutionRepository institutionRepository,
+                          DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model){
         model.addAttribute("institutions", institutionRepository.findAll());
+
+//        model.addAttribute("numberOfDonations", donationRepository.count());
+        model.addAttribute("numberOfDonations", donationRepository.sumQuantity());
         return "index";
     }
 }
--- src/main/java/pl/coderslab/charity/donation/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/donation/DonationRepository.java
@@ -1,7 +1,9 @@
 package pl.coderslab.charity.donation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DonationRepository extends JpaRepository<Donation, Long> {
-
+    @Query("SELECT sum(d.quantity) from Donation d")
+    int sumQuantity();
 }
