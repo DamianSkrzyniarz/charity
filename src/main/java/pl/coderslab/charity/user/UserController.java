685d2e88f9e81de4a3b66e6dae4de43977f56a1c
Damian Skrzyniarz <skrzyniarz.damian@gmail.com>
2019-08-09 18:33:35 +0200

    Added registration form, created necessary entities, repositories and services

---
+++ src/main/java/pl/coderslab/charity/user/UserController.java
@@ -0,0 +1,31 @@
+package pl.coderslab.charity.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/register")
+    public String register(Model model){
+        model.addAttribute("user", new User());
+        return "/register";
+    }
+
+    @PostMapping("/register")
+    public String registerPost(User user) {
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
+
+}
